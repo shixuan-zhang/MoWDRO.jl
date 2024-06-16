ecdc2fd16063a637bca90ce301183e1c493acb1c
Shixuan Zhang <zhangshixuanus@gmail.com>
2024-06-15 21:56:06 -0500

    Initialize source file

--- src/MoWDRO.jl
+++ src/MoWDRO.jl
@@ -1,5 +1,17 @@
+# Moment relaxations for Wasserstein Distributionally Robust Optimization
 module MoWDRO
 
-# Write your package code here.
+# import basic mathematics and statistics modules
+import LinearAlgebra, Random, Statistics
+
+# define module-wide shared parameters
+const NUM_DIG = 8
+const VAL_TOL = 1.0e-8
+const VAL_INF = 1.0e9
+const NUM_MAX_ITER = 1000
+
+# include module types and methods
+
+# include algorithms and relaxations
 
 end
